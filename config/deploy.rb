9586be845353216bbe604a13bb72593c6d6eef5a
Eldar <eldar@telega.in>
2024-01-11 05:30:43 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,7 @@ set :puma_workers, 4
 
 set :user, 'deployer'
 set :pty, true
-#set :use_sudo, true
-#set :sudo_user, "deployer"
+set :use_sudo, false
 set :stage, :production
 set :deploy_via, :remote_cache
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch :application}"
@@ -21,7 +20,7 @@ set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log, "#{release_path}/log/puma.error.log"
-set :ssh_options, { forward_agent: true } # , user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true # Change to false when not using ActiveRecord
